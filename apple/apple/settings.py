055fd893874434984d4f6cfe5d7ae1de65fcc994
wendy <wendy@wendy-X556UR>
2019-01-17 21:25:22 +0800

    1-17-1

--- apple/apple/settings.py
+++ apple/apple/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
 ]
 
 MIDDLEWARE = [
@@ -81,6 +82,7 @@ DATABASES = {
         'PASSWORD': 'apple',
         'HOST': 'localhost',
         'PORT': '',
+
     }
 }
 
