0f6c504889627a760ea87a548a29b37eedb0493e
wendy <wendy@wendy-X556UR>
2019-01-19 01:04:45 +0800

    1-18-3

--- apple/article/views.py
+++ apple/article/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from article.models import Article, Comment
+from article.forms import ArticleForm
 
 def article(request):
     '''
@@ -10,3 +12,23 @@ def article(request):
         articles.update({article:Comment.objects.filter(article=article)})
     context = {'articles':articles}
     return render(request, 'article/article.html', context)
+
+def articleCreate(request):
+    '''
+    Create a new article instance
+        1. If method is GET, render an empty form
+        2. If method is POST, perform form validation and display error messages if the form is invalid
+        3. Save the form to the model and redirect the user to the article page
+    '''
+    template = 'article/articleCreate.html'
+    if request.method == 'GET':
+        print(ArticleForm())
+        return render(request, template, {'articleForm':ArticleForm()})
+        # POST
+    articleForm = ArticleForm(request.POST)
+    if not articleForm.is_valid():
+        return render(request, template, {'articleForm':articleForm})
+
+    articleForm.save()
+    messages.success(request, '文章已新增')
+    return redirect('article:article')
