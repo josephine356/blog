25cc090a55ae3dbd23cbd692b9cac6b4e3d3aae0
momo94204 <46774611+momo94204@users.noreply.github.com>
2019-01-20 18:56:50 +0800

    Delete production.py

--- apple/populateeee/production.py
+++
@@ -1,8 +0,0 @@
-from populateee import base
-
-from account.models import User
-
-
-print('Creating apple account ... ', end='')
-User.objects.create_superuser(username='apple', password='apple10614063, email=None, fullName='管理者')
-print('done')
