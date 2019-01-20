41234f262ab45aab0c73373b0e6a6153d9c27404
momo94204 <46774611+momo94204@users.noreply.github.com>
2019-01-20 17:53:23 +0800

    Create production.py

---
+++ apple/populateeee/production.py
@@ -0,0 +1,8 @@
+from populate import base
+
+from account.models import User
+
+
+print('Creating apple account ... ', end='')
+User.objects.create_superuser(username='apple', password='apple10614063, email=None, fullName='管理者')
+print('done')
