b5753f7ed6e0d49ddbb455ff25c1a37e46330073
momo94204 <46774611+momo94204@users.noreply.github.com>
2019-01-20 16:54:24 +0800

    Update settings.py

--- apple/apple/settings.py
+++ apple/apple/settings.py
@@ -85,7 +85,7 @@ if DEBUG:   # Running on the development environment
 
     }
 }
-    else:   # Running on Heroku
+else:   # Running on Heroku
     # Parse database configuration from $DATABASE_URL
     import dj_database_url
     DATABASES = {'default':dj_database_url.config()}
